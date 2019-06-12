3efe0d278e459b830338f9c7d0c0f636d2ba86c1
xiaoerhao <1193435629@qq.com>
2019-06-12 14:48:46 +0800

    First commit

---
+++ Blog.Core/Blog.Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Model
+{
+    /// <summary>
+    /// 用户表
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
